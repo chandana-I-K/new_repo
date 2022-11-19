b906e90012f5a9ac3b9cf4a1daf25bd5c5212654
chandana-I-K <chandana.ik@robosoftin.com>
2022-11-19 11:52:29 +0530

    frst commit

---
+++ src/main/java/com/trial1/trial1/entity/a.java
@@ -0,0 +1,11 @@
+package com.trial1.trial1.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class a {
+}
---
+++ src/main/java/com/trial1/trial1/service/Aservice.java
@@ -0,0 +1,7 @@
+package com.trial1.trial1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Aservice {
+}
