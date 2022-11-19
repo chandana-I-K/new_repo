e052bf23ca12a2dde93c3ceaa51899717610dd2e
chandana-I-K <chandana.ik@robosoftin.com>
2022-11-19 11:53:45 +0530

    first commit

---
+++ src/main/java/com/trial2/trial2/Trial2Application.java
@@ -0,0 +1,13 @@
+package com.trial2.trial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trial2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/trial2/trial2/Trial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trial2.trial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
